1d74e46bef3387caa77da4006be958e9499e7ddf
Christopher Burnett <cburnett@cops-022690.uoa.abdn.ac.uk>
2015-03-18 16:02:45 +0000

    More work on the controller - not tested at all as of yet. Should be able to post JSON for a user (giving the user_id - oh dear, insecure...) and have it created. Could test later. The idea is that this will match the resource pattern in the angular/ionic frontend and everything will be nice and smooth

--- backend/app/controllers/qids_responses_controller.rb
+++ backend/app/controllers/qids_responses_controller.rb
@@ -1,2 +1,17 @@
 class QidsResponsesController < ApplicationController
+  def create
+    user = User.find(params[:user_id])
+    if user then
+      user.qids_responses.create(safe_params)
+      render json: task, status: 201
+    end
+  end
+
+  private
+  
+  def safe_params
+    params.require(:qids_response).permit(:user_id,
+                                          :completed_at, :q1, :q2, :q3, :q4, :q5, :q6, :q7, :q8, :q9, :q10,
+                                          :q11, :q12, :q13, :q14, :q15, :q16, :score)
+  end
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ActiveRecord::Base
+  has_many :qids_responses
 end
