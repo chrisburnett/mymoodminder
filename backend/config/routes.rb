ebeea752eee6e58e55aec1770d986a8404110910
Christopher Burnett <cburnett@cops-022690.uoa.abdn.ac.uk>
2015-03-18 15:54:09 +0000

    Restrict routes created for the resource to only those we'll need - don't need rails UI routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users do
-      resources :qids_responses
+      resources :qids_responses, only: [:index, :create, :update, :destroy]
     end
   end
   
