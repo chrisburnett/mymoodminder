93f0232ffd3b16b72200ec12920b10d49bbc43fa
Chris Burnett <dameramu@gmail.com>
2015-03-21 08:17:20 +0000

    Add new fields to the backend user model, including the schema.

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150320135931) do
+ActiveRecord::Schema.define(version: 20150320223634) do
 
   create_table "qids_responses", force: :cascade do |t|
     t.integer  "user_id"
@@ -40,8 +40,10 @@ ActiveRecord::Schema.define(version: 20150320135931) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "username"
+    t.string   "username",        null: false
     t.string   "password_digest"
+    t.string   "forename"
+    t.string   "surname"
   end
 
 end
