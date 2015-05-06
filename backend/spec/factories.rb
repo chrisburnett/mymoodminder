c0a388276f2298a1198e2104decdca8370b55916
Christopher Burnett <cburnett@cops-022690.uoa.abdn.ac.uk>
2015-05-06 14:10:48 +0100

    Updated factories

--- backend/spec/factories.rb
+++ backend/spec/factories.rb
@@ -1,8 +1,19 @@
 FactoryGirl.define do
 
   factory :qids_response do
-    q1 "a"
-    q2 "b"
+    q1 "2"
+    q2 "2"
+    q3 "2"
+    q5 "2"
+    q6_7 "2"
+    q8_9 "2"
+    q10 "2"
+    q11 "2"
+    q12 "2"
+    q13 "2"
+    q14 "2"
+    q15 "2"
+    q16 "2"
     score "20"
     completed_at "2015-03-21T12:24:26.000Z"
     user
