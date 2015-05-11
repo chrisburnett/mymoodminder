cdb628998f46a0cfdab1b58657ac092e33de5a5b
Chris Burnett <dameramu@gmail.com>
2015-05-11 01:09:41 +0100

    Added messages model to backend

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150320223634) do
+ActiveRecord::Schema.define(version: 20150508010709) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id"
 
   create_table "qids_responses", force: :cascade do |t|
     t.integer  "user_id"
@@ -40,7 +49,7 @@ ActiveRecord::Schema.define(version: 20150320223634) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "username",        null: false
+    t.string   "username"
     t.string   "password_digest"
     t.string   "forename"
     t.string   "surname"
