de95ea587c8331313c7cc016fedf2cd88b1bcd95
Chris Burnett <dameramu@gmail.com>
2015-05-21 14:36:44 +0100

    Updated schema with registration ID for users and log events

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150520115855) do
+ActiveRecord::Schema.define(version: 20150521104426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "log_events", force: :cascade do |t|
+    t.text     "content"
+    t.string   "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -135,6 +142,7 @@ ActiveRecord::Schema.define(version: 20150520115855) do
     t.string   "forename"
     t.string   "surname"
     t.boolean  "admin"
+    t.string   "registration_id"
   end
 
   add_foreign_key "messages", "presets"
