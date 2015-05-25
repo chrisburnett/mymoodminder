cbf59503210bd377c77859a64e0b3e894f91de7d
Chris Burnett <dameramu@gmail.com>
2015-05-25 16:36:41 +0100

    Message JSON output includes the category

--- backend/app/models/message.rb
+++ backend/app/models/message.rb
@@ -2,9 +2,18 @@ class Message < ActiveRecord::Base
   belongs_to :user
   belongs_to :preset
   belongs_to :user, foreign_key: 'sender_id'
+  
+  def as_json(options)
+    json = super(options)
+    #if the message has a preset, append its category also, if the
+    # message has a preset, the preset content will take precedence
+    # over any explicitly defined content
+    if preset then
+      json[:category] = preset.category.title
+      json[:content] = preset.content
+    end
 
-  
-  
-  
+    return json
+  end
   
 end
