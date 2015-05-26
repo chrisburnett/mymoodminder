50f65f01fcb7965e7f9bee5f4e9f256c4a7cdf3b
Chris Burnett <dameramu@gmail.com>
2015-05-26 17:45:15 +0100

    Change to use status code symbols

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -2,7 +2,7 @@ class Api::UsersController < SecureAPIController
 
   def index
     users = User.all
-    render json: users, status: 201
+    render json: users, status: :ok
   end
 
   # set the user's device ID for pushing notifications to Google Cloud
@@ -10,8 +10,7 @@ class Api::UsersController < SecureAPIController
   def register
     if @current_user then
       @current_user.update_attribute(:registration_id, params[:registration_id])
-      render json: @current_user.registration_id, status: 201
-      puts @current_user.errors.messages
+      render json: @current_user.registration_id, status: :ok
     else
       fail NotAuthenticatedError
     end
