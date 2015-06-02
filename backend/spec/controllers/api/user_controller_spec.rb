d46ff7aa143cf9d8bbb09590dab96d0a11b07715
Chris Burnett <dameramu@gmail.com>
2015-06-02 16:42:57 +0100

    Spec for Api::User

---
+++ backend/spec/controllers/api/user_controller_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+require 'spec_helper'
+
+RSpec.describe Api::UsersController, :type => :controller do
+
+  before(:each) do
+    @current_user = create(:user)
+    controller.instance_variable_set(:@current_user, @current_user)
+  end
+
+  describe "GET index" do
+    it "responds with HTTP 200" do
+      get :show
+      expect(response.status).to be 200
+    end
+    
+    it "responds with the user's profile" do
+      get :show
+      expect(JSON.parse(response.body)['forename']).to eq("Chris")
+    end
+  end
+
+  describe "PUT index" do
+    it "sets the current user's notification flag" do
+      put :update, { user: { receive_notifications: false } }
+      expect(JSON.parse(response.body)['receive_notifications']).to be_falsey
+    end
+  end
+
+end
