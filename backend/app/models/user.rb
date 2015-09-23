b161d0cb73c0fdbc5abe925492002b6c947e8cab
Chris Burnett <dameramu@gmail.com>
2015-09-24 00:33:20 +0100

    Don't export passwords

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -92,9 +92,10 @@ class User < ActiveRecord::Base
 
   def self.to_csv
     CSV.generate do |csv|
-      csv << column_names
+      cols = column_names.reject { |c| c == 'password_digest' }
+      csv << cols
       all.each do |user|
-        csv << user.attributes.values_at(*column_names)
+        csv << user.attributes.values_at(*cols)
       end
     end
   end
