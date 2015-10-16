b0bad936e7ec52232fb4244279d10d818625b0fa
VINCENT BODINIER <vbodinier@gmail.com>
2015-10-16 09:49:47 +0100

    fix and improvement of the reset QIDS timer functionality

--- backend/app/controllers/admin/users_controller.rb
+++ backend/app/controllers/admin/users_controller.rb
@@ -2,14 +2,22 @@ class Admin::UsersController < ApplicationController
 
   def update
     # just some convenience setting here for user attributes
-
     u = User.find(safe_params[:user_id])
     if safe_params[:next_delivery_time] == 'reset' then
       u.next_delivery_time = Time.now
     end
-    if safe_params[:next_qids_reminder_time] == 'reset' then
-      u.next_qids_reminder_time = Time.now
+	
+	if safe_params[:next_qids_reminder_time] && DateTime.parse(safe_params[:next_qids_reminder_time]) then
+	  #ex: 13/10/2015 09:10:00 +0100'
+	  gmtFlag = " +0100"
+	  date = DateTime.parse(safe_params[:next_qids_reminder_time] + gmtFlag)
+      u.next_qids_reminder_time = date
     end
+    #if safe_params[:next_qids_reminder_time] == 'reset' then
+    #  u.next_qids_reminder_time = Time.now
+	#  #u.next_qids_reminder_time = DateTime.tomorrow
+    #end
+	
     if safe_params[:gp_contact_number] then
       u.gp_contact_number = safe_params[:gp_contact_number]
     end
@@ -41,7 +49,7 @@ class Admin::UsersController < ApplicationController
 
 
   private
-
+  
   def safe_params
     params.require(:user).permit(:id, :user_id, :next_delivery_time, :next_qids_reminder_time, :gp_contact_number)
   end
